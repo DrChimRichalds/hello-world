b2193657833dd5a7fd0e05c453a37adb3268c70e
DrChimRichalds <cdyhockey50@gmail.com>
2026-09-08 00:02:28 +0000

    07SEP2026

--- Python_Fundamentals_1/ASCII_Art_Code.py
+++ Python_Fundamentals_1/ASCII_Art_Code.py
@@ -1,22 +1,68 @@
 # Python code​​​​​‌​​​​​​​‌‌​‌‌​‌​‌​​‌​‌​​‌​ below
 
 def encodeString(myword):
-    print(myword.split())
-    new_list = [(char, myword.count(char)) for char in set(myword)]
-    print(new_list)
-    
-def decodeString(encodedList):
-    # Your code goes here.
-    result = ""
-    for char, count in encodedList:
-        result = result + char*count
-        print(result)
+    #new_list = [(char, myword.count(char)) for char in set(myword)]
+    #don't want .count because that doesn't retain the order 
+    #new_list.sort(key=lambda x: myword.index(x[0]))
+    PrevChar = myword[0]
+    count = 0
+    new_list = []
+    for char in myword: 
+        if char != PrevChar:
+            new_list.append((PrevChar, count))
+            count = 0
+        PrevChar = char
+        count = count + 1
+    new_list.append((PrevChar, count))
+    return print(new_list)
 
-encodeString('AAACCCCAA')
 
-decodeString([('A', 4), ('C', 4), ('D', 3)])
+input_test = 'Bookkeeper'
+
+#decodeString([('\n', 37), (' ', 2211), ('%', 510)])
 
 #it'd be easy to iterate through and craete
 #  a new list that represents the string as a list, 
 # then i wonder if I can iterate through a list and 
 # create a count in a dictionary where the value + 
+
+art = '''
+
+                                                                                
+                                                                                
+                               %%%%%%%%%%%%%%%%%%%                              
+                        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                       
+                    %%%%%%%%                         %%%%%%%%                   
+                %%%%%%%                                   %%%%%%                
+              %%%%%%                                         %%%%%%             
+           %%%%%%                                               %%%%%           
+          %%%%%                                                   %%%%%         
+        %%%%%                                                       %%%%%       
+       %%%%                 %%%%%              %%%%%                  %%%%      
+      %%%%                 %%%%%%%            %%%%%%%                  %%%%     
+     %%%%                  %%%%%%%            %%%%%%%                   %%%%    
+    %%%%                   %%%%%%%            %%%%%%%                    %%%%   
+    %%%%                    %%%%%              %%%%%                     %%%%   
+   %%%%                                                                   %%%%  
+   %%%%                                                                   %%%%  
+   %%%%                                                                   %%%%  
+   %%%%                                                      %%%%        %%%%   
+    %%%%       %%%%%%                                        %%%%%       %%%%   
+    %%%%         %%%%                                       %%%%        %%%%    
+     %%%%         %%%%                                     %%%%         %%%%    
+      %%%%         %%%%%                                  %%%%         %%%%     
+       %%%%%         %%%%%                             %%%%%         %%%%%      
+        %%%%%          %%%%%%                        %%%%%          %%%%        
+          %%%%%           %%%%%%%               %%%%%%%           %%%%%         
+            %%%%%             %%%%%%%%%%%%%%%%%%%%%             %%%%%           
+              %%%%%%%                                        %%%%%              
+                 %%%%%%%                                 %%%%%%%                
+                     %%%%%%%%%                     %%%%%%%%%                    
+                          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%                         
+                                   %%%%%%%%%%%%                                 
+                                                                                
+                                                                                 
+
+'''
+encoded = encodeString(art)
+#decoded = decodeString(encoded)
